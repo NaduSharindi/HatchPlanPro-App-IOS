c8249bb8978180472c7de27f6fcdf44de2d3ef67
NaduSharindi <nadusharindi@gmail.com>
2026-05-04 15:38:43 +0530

    Initial Commit

---
+++ HatchPlan Pro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HatchPlan Pro
+//
+//  Created by COBSCCOMP242P-063 on 2026-05-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HatchPlan Pro/HatchPlan_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  HatchPlan_ProApp.swift
+//  HatchPlan Pro
+//
+//  Created by COBSCCOMP242P-063 on 2026-05-04.
+//
+
+import SwiftUI
+
+@main
+struct HatchPlan_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
